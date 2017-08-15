ac5d0c26a9bbd12434f20e8d506c1fa185f10334
jagadeeshmarthy <jagan.marthy@gmail.com>
2017-08-15 15:36:15 +0530

    changed cred

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const MoltinGateway = require('@moltin/sdk').gateway;
 exports.Moltin = MoltinGateway({
-  client_id: 'XXXXXXXXXXXXXX',
-  client_secret: 'XXXXXXXXXXXXXXXXX',
+  client_id: 'O7VXR9S8yguuFRGAr0MVntxV1S81xmSSz6TTJwtzSc',
+  client_secret: 'Xonz2lZne7fNmeLbrOemCvHAm9W9bRweW1snXHFw5L',
 });
 
 exports.mongodbUri = 'localhost:27017/dev'
